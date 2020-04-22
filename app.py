a529f1775ca78c8d26adff7a65f819b6b6e92eb6
Niroop Sriram <37906701+niroop007@users.noreply.github.com>
2020-04-22 15:38:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
-import numpy as np
-from flask import Flask,request, render_template, url_for
-import tensorflow as tf
-from tensorflow import keras
-from keras.models import load_model
-from werkzeug.utils import secure_filename
-from keras.preprocessing import image
-
+from flask import Flask,request, render_template
 
 
 app = Flask(__name__)
-#model = keras.models.load_model('Covid_Vgg.h5')
 @app.route("/")
 def index():
     #return render_template('index.html')
